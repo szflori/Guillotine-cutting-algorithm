98100fffdf251ec07bf1918bb52200395b5b7436
Florian Szmolenicki <szflorian97@gmail.com>
2025-05-05 23:32:34 +0200

    Fix scroll

--- app/src/main/java/com/example/guillotine_cutting_app/MainActivity.kt
+++ app/src/main/java/com/example/guillotine_cutting_app/MainActivity.kt
@@ -4,10 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.rememberNavController
 import com.example.guillotine_cutting_app.navigation.AppNavGraph
 import com.example.packingoptimizerapp.android.ui.layouts.TopBar
@@ -21,7 +27,10 @@ class MainActivity : ComponentActivity() {
 
             Scaffold(topBar = { TopBar(title = "Optimalizálás", navController = navController) }) {
                 innerPadding ->
-                Column(modifier = Modifier.padding(innerPadding)) {
+                Column(modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding),
+                ) {
                     AppNavGraph(navController)
                 }
             }
--- app/src/main/java/com/example/guillotine_cutting_app/ui/screens/CuttingScreen.kt
+++ app/src/main/java/com/example/guillotine_cutting_app/ui/screens/CuttingScreen.kt
@@ -7,10 +7,13 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
@@ -73,6 +76,7 @@ fun CuttingScreen(navController: NavController) {
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .verticalScroll(rememberScrollState())
             .padding(16.dp),
         verticalArrangement = Arrangement.spacedBy(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
@@ -111,11 +115,11 @@ fun CuttingScreen(navController: NavController) {
         ) {
             Text("Lapok", style = MaterialTheme.typography.titleMedium)
 
-            LazyColumn(
+            Column(
                 modifier = Modifier.fillMaxWidth(),
-                verticalArrangement = Arrangement.spacedBy(8.dp)
+            verticalArrangement = Arrangement.spacedBy(8.dp)
             ){
-                itemsIndexed(lapList) { index, lap ->
+                lapList.forEachIndexed { index, lap ->
                     Column(
                         modifier = Modifier.fillMaxWidth()
                             .padding(vertical = 8.dp),
